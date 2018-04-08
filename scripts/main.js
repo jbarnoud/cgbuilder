ae9260d1287dc4d7aaae4f054daced944852cb07
Jonathan Barnoud <jonathan@barnoud.net>
2018-04-08 13:14:38 +0200

    Prevent move on click selection
    Starting with NGL v2.0.0-dev.11, the click selection is bounded to the
    centering on the selected atom. This commit deactivate that behaviour.

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,12 @@ function main() {
     // Load PDB entry 1CRN
     //stage.loadFile( "rcsb://1crn", { defaultRepresentation: true } );
 	stage.loadFile("data/benzene_atb.pdb", { defaultRepresentation: true } );
+	// Remove preset action on atom pick.
+	// As of NGL v2.0.0-dev.11, the left click atom pick is binded to the
+	// centering of the view on the selected atom. In previous versions, this
+	// behavior was linked on shift-click, instead.
+	stage.mouseControls.remove("clickPick-left");
+	// Bind our own selection beheviour.
     stage.signals.clicked.add(function (pickingProxy) {
         beads.push(pickingProxy.atom); console.log(beads.length)
     });
